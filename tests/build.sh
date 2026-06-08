8eb5ef8fe06b914807a65dbbde6cac29182c91e3
Andrei Smirnov <andrei.smirnov.69@gmail.com>
2026-06-08 00:16:59 -0400

    add cmakes

--- tests/build.sh
+++
@@ -1,7 +0,0 @@
-rm *.o *.tsk
-g++ --std=c++20 -g -c -I ../../libjupiterli/cpp ../../libjupiterli/cpp/jupiterli.cpp
-g++ --std=c++20 -g -c -I ../../libjupiterli/cpp producer.cpp
-g++ --std=c++20 -g producer.o jupiterli.o -o producer.tsk
-
-g++ --std=c++20 -g -c -I ../../libjupiterli/cpp agent.cpp
-g++ --std=c++20 -g agent.o -o agent.tsk
